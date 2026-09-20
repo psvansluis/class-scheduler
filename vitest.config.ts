16a2f085ae09a9f9689ee7822075e62dab51edce
psvansluis <psvansluis@users.noreply.github.com>
2026-09-20 21:19:51 +0200

    simplify tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["test/{prolog,unit}/**/*.{spec,test}.ts"],
+    include: ["test/unit/**/*.{spec,test}.ts"],
   },
 });
