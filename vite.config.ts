c78b869d5a600293975d7d83a43f7f8268d19a8d
psvansluis <psvansluis@users.noreply.github.com>
2026-09-25 22:46:07 +0200

    prettier

--- vite.config.ts
+++ vite.config.ts
@@ -92,8 +92,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   base: "/class-scheduler/",
 });
